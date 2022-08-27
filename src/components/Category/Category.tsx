7f4b52b6f1b080d60c03bca5c766c43d618c4fe6
Omurbek <firefoxer00@gmail.com>
2022-08-27 16:32:13 +0600

    add dropdown for category/component

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -1,10 +1,12 @@
-import React from "react"
+import React, { useState } from "react"
 
 const Category = () => {
+  const [open, isOpen] = useState<boolean>(false)
+
   return (
     <section className="pt-2">
       <div className="w-full flex flex-col py-4">
-        <div className="flex flex-col items-center  md:flex-row lg:mb-0 lg:items-center py-2 z-[5]">
+        <div className="hidden  flex-col items-center md:flex md:flex-row lg:mb-0 lg:items-center py-2 z-[5]">
           <button className="btn-category shadow-btn-active">Любимое</button>
           <button className="btn-category">Закуски</button>
           <button className="btn-category">Салаты</button>
@@ -13,6 +15,27 @@ const Category = () => {
           <button className="btn-category">Десткое</button>
           <button className="btn-category">Десерты</button>
           <button className="btn-category">Напитки</button>
+        </div>
+
+        {/* Dropdown menu */}
+
+        <button
+          onClick={() => isOpen(!open)}
+          className="w-[70%] mx-auto py-3 px-7 bg-slate-400 md:hidden"
+        >
+          Категорий
+        </button>
+
+        <div
+          className={`${
+            open ? `block` : `hidden`
+          } w-[70%] mx-auto bg-slate-500 md:hidden`}
+        >
+          <ul>
+            <li>Любимое</li>
+            <li>Салаты</li>
+            <li>Закуски</li>
+          </ul>
         </div>
       </div>
     </section>
