7ccdd57bd5448595f7ceb28d478e14c69ea57211
Omurbek <firefoxer00@gmail.com>
2022-08-29 14:45:38 +0600

    Create data file

---
+++ src/data.ts
@@ -0,0 +1,16 @@
+import IFood from "./types/FoodData"
+
+export const data: IFood[] = [
+  {
+    id: "1",
+    name: "Кукси",
+    cal: { calories: 120, gram: 300 },
+    price: 100,
+    description: "lorem10",
+    category: "salad",
+    privatePerson: false,
+    corporatePerson: false,
+    image:
+      "https://images.unsplash.com/photo-1607532941433-304659e8198a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1078&q=80",
+  },
+]
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit"
 
+// const persistConfig = {
+// key: "root",
+// storage,
+// }
+
+// const persistedReducer = persistReducer(persistConfig, {})
+
 export const store = configureStore({
   reducer: {},
 })
+
+// export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
