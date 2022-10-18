fe3b55e49439144e9448ec367c338ea63b53b604
Omurbek <firefoxer00@gmail.com>
2022-10-18 21:12:23 +0600

    add Status component in order-detail/page

--- src/app/pages/OrderDetail/index.tsx
+++ src/app/pages/OrderDetail/index.tsx
@@ -4,6 +4,7 @@ import { useAppSelector } from "../../hooks"
 import { IAddressData } from "@base/app/types/userProfile"
 import Row from "./Row"
 import FoodsList from "./FoodsList"
+import { FC, ReactNode } from "react"
 
 const OrderDetail = () => {
   const params = useParams<string>()
@@ -12,7 +13,7 @@ const OrderDetail = () => {
       (item) => item.id.toString() === params.id
     )
   )
-  const { id, date, totalSum, address, foods, deliveryPrice } = order
+  const { id, date, totalSum, address, foods, deliveryPrice, status } = order
   const {
     street,
     houseNumber,
@@ -29,6 +30,8 @@ const OrderDetail = () => {
         <Title titleName="Деталь заказа" toPath="/orders" />
 
         <div>
+          <OrderStatus type={status} />
+
           <Row title="Номер заказа" data={id} />
           <Row title="Время оформления заказа" data={date} />
           <Row title="Способ оплаты" data={"Наличными"} />
@@ -56,6 +59,30 @@ const OrderDetail = () => {
       </section>
     </Wrapper>
   )
+}
+
+type OrderStatusProps = {
+  type: "current" | "delivered" | "rejected"
+}
+const OrderStatus: FC<OrderStatusProps> = ({ type }): any => {
+  if (type === "current")
+    return (
+      <h1 className="text-[20px] text-center font-bold py-4 text-yellow-600">
+        Текущий заказ
+      </h1>
+    )
+  if (type === "delivered")
+    return (
+      <h1 className="text-[20px] text-center font-bold py-4 text-green-600">
+        Доставлен
+      </h1>
+    )
+  if (type === "rejected")
+    return (
+      <h1 className="text-[20px] text-center font-bold py-4 text-red-600">
+        Отменен
+      </h1>
+    )
 }
 
 export default OrderDetail
