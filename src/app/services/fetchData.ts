d449b12c495d02f2d7a2716d3fad3831928c9e36
Omurbek <firefoxer00@gmail.com>
2022-10-21 17:04:20 +0600

    change base url in axios

--- src/app/services/fetchData.ts
+++ src/app/services/fetchData.ts
@@ -1,16 +1,16 @@
 import axios from "axios"
 import { IFood } from "../types"
 
-const API_URL = "http://localhost:3004"
+const API_URL = "https://omur-api.herokuapp.com/api/cookservice"
 
 axios.defaults.baseURL = API_URL
 
 export async function fetchFoods(): Promise<IFood[]> {
-  const { data } = await axios.get("/allfood")
-  return data
+  const { data } = await axios.get("/products")
+  return data.products
 }
 
 export async function fetchCategories(): Promise<any[]> {
-  const { data } = await axios.get("/datacategory")
-  return data
+  const { data } = await axios.get("/category")
+  return data.dataCategory
 }
