35a5804b3f9f349e503f0e5a57155bdca4b6992b
Omurbek <firefoxer00@gmail.com>
2022-10-22 14:08:15 +0600

    add scroll-up when change page

--- src/app/pages/Layout.tsx
+++ src/app/pages/Layout.tsx
@@ -1,7 +1,14 @@
-import { Outlet } from "react-router-dom"
+import { useEffect } from "react"
+import { Outlet, useLocation } from "react-router-dom"
 import { Footer, Toast, PushUpOrder, Header } from "../components"
 
 const Layout = () => {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
   return (
     <>
       <Header />
