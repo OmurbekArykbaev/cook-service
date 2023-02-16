c8e62f0e3296652320af212f6d9dfc9624b96d69
Omurbek <firefoxer00@gmail.com>
2023-02-16 14:50:01 +0600

    fetch from mock api, add const data in project

--- src/app/services/fetchData.ts
+++ src/app/services/fetchData.ts
@@ -1,16 +1,16 @@
 import axios from "axios"
 import { IFood } from "../types"
 
-const API_URL = "https://omur-api.herokuapp.com/api/cookservice"
+const API_URL = "https://mocki.io/v1"
 
 axios.defaults.baseURL = API_URL
 
 export async function fetchFoods(): Promise<IFood[]> {
-  const { data } = await axios.get("/products")
+  const { data } = await axios.get("/9d5a600a-02fb-4f34-8116-e41344c844a9")
   return data.products
 }
 
 export async function fetchCategories(): Promise<any[]> {
-  const { data } = await axios.get("/category")
+  const { data } = await axios.get("/1a621da8-2107-4b9b-9636-4ae28c5e29b3")
   return data.dataCategory
 }
